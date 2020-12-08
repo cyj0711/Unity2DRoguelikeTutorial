92720843a2dd3dc43012e4d255121b09fdb846d3
cyj0711 <ddk0711.gmail.com>
2020-12-08 13:18:30 +0900

    Add BoardManager, GameManager Scripts.

---
+++ Assets/Scripts/BoardManager.cs
@@ -0,0 +1,107 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using UnityEngine;
+using Random=UnityEngine.Random;
+
+public class BoardManager : MonoBehaviour
+{
+    [Serializable]
+    public class Count
+    {
+        public int minimum;
+        public int maximum;
+
+        public Count(int min, int max)
+        {
+            minimum = min;
+            maximum = max;
+        }
+    }
+
+    // size of map
+    public int columns = 8;
+    public int rows = 8;
+
+    // each map has 5~9 walls and 1~5 food
+    public Count wallCount = new Count(5, 9);
+    public Count foodCount = new Count(1, 5);
+
+    public GameObject exit; // exit is only one
+    public GameObject[] floorTiles;
+    public GameObject[] wallTiles;
+    public GameObject[] foodTiles;
+    public GameObject[] enemyTiles;
+    public GameObject[] outerWallTiles;
+
+    private Transform boardHolder;  // make clean the Hierarchy
+    private List<Vector3> gridPositions = new List<Vector3>();  // detects all of locations as possible. Also track whether an object is in place or not.
+
+    void InitializeList()
+    {
+        gridPositions.Clear();
+
+        // edge lines must be floor tile. So, game is always possible to clear.
+        for (int x = 1; x < columns - 1; x++)
+        {
+            for (int y = 1; y < rows - 1; y++)
+            {
+                gridPositions.Add(new Vector3(x, y, 0f));
+            }
+        }
+    }
+
+    void BoardSetup()
+    {
+        boardHolder = new GameObject("Board").transform;
+
+        for (int x = -1; x < columns + 1; x++)
+        {
+            for (int y = -1; y < rows + 1; y++)
+            {
+                GameObject toInstantiate = floorTiles[Random.Range(0, floorTiles.Length)];
+
+                // outerwall locations
+                if (x == -1 || x == columns || y == -1 || y == rows)
+                    toInstantiate = outerWallTiles[Random.Range(0, outerWallTiles.Length)];
+
+                GameObject instance = Instantiate(toInstantiate, new Vector3(x, y, 0f), Quaternion.identity) as GameObject;
+
+                instance.transform.SetParent(boardHolder);
+            }
+        }
+    }
+
+    Vector3 RandomPosition()
+    {
+        int randomIndex = Random.Range(0, gridPositions.Count);
+        Vector3 randomPosition = gridPositions[randomIndex];
+        gridPositions.RemoveAt(randomIndex);    // delete the index to protect from duplicated position
+        return randomPosition;
+    }
+
+    void LayoutObjectAtRandom(GameObject[] tileArray, int minimum, int maximum)
+    {
+        int objectCount = Random.Range(minimum, maximum + 1);
+
+        for (int i = 0; i < objectCount; i++)
+        {
+            Vector3 randomPosition = RandomPosition();
+            GameObject tileChoice = tileArray[Random.Range(0, tileArray.Length)];
+            Instantiate(tileChoice, randomPosition, Quaternion.identity);
+        }
+
+    }
+
+    // This function called by GameManager when game board is made(So, this function is public)
+    public void SetupScene(int level)
+    {
+        BoardSetup();
+        InitializeList();
+        LayoutObjectAtRandom(wallTiles, wallCount.minimum, wallCount.maximum);
+        LayoutObjectAtRandom(foodTiles, foodCount.minimum, foodCount.maximum);
+        int enemyCount = (int)Mathf.Log(level, 2f); // enemy count is increased by level with log2 function.
+        LayoutObjectAtRandom(enemyTiles, enemyCount, enemyCount);
+        Instantiate(exit, new Vector3(columns - 1, rows - 1, 0f), Quaternion.identity); // Exit is located at top of right corner.
+    }
+}
---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public static GameManager instance = null;  // make GameManager singleton. (any scripts can access public functions and variables of GameManager)
+    public BoardManager boardScript;
+
+    private int level = 3;
+
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance == null)
+            instance = this;
+        else if (instance != this)  // destroy the gameManager not to make double gameManager
+            Destroy(gameObject);
+
+        // all of objects in hierarchy is destroyed when new scene is loaded
+        // but GameManager has to keep calculating the score even though the scene is over.
+        // So, used 'DontDestroyOnLoad' function to protect GameManager from destroying.
+        DontDestroyOnLoad(gameObject);
+        boardScript = GetComponent<BoardManager>();
+        InitGame();
+    }
+
+    void InitGame()
+    {
+        boardScript.SetupScene(level);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
